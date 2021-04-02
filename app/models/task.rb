962576732c7ee53aedf336429b03612b4df02bc7
manoj <manoj.itquest@gmail.com>
2021-04-03 00:10:14 +0530

    added validation for status

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,6 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :description, :status, :deadline
+  validates :status, inclusion: { in: %w[backlog inprogress done] }
+
 end
